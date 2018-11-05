1fd1af2cd17f0ffb7033adc9bde745d09c908495
Emely <mly.molina.minaya@gmail.com>
2018-11-05 14:43:53 -0800

    port changed

--- server/index.js
+++ server/index.js
@@ -5,16 +5,12 @@ const path = require('path');
 const db = require('../db/index');
 
 
-const PORT = 3000;
+const PORT = 3002;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/restaurants/:restaurantID/', express.static(path.join(__dirname, '../client/dist/')));
-
-// app.get('/restaurants/:restaurantID/', (req, res) => {
-
-// });
 
 app.get('/restaurants/:restaurantID/reviews', (req, res) => {
   const restaurantId = req.params.restaurantID;
